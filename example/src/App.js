cb94167e7f34e70b13b7a7ef11acec1267496ec9
Kadd <kadirkavraz@gmail.com>
2023-04-24 01:32:50 +0300

    filesAdded

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,23 @@
 import React from 'react'
 
-import { ExampleComponent } from 'kaddbutton0'
+import { Button } from 'kaddbutton0'
 import 'kaddbutton0/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+  <div>
+
+  <Button text='Primary Button' type='primary' />
+
+  <Button text='Default Button ' type='default' />
+
+  <Button text='Dashed Button' type='dashed' />
+ 
+  <Button text='Text Button' type='text' />
+  
+  <Button text='Link Button' type='link' />
+</div>
+  )
 }
 
 export default App
